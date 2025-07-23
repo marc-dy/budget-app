c8d7da534b9541a39d065c27a605c88970a01078
Marc Dy <marc.dy1989@gmail.com>
2025-07-23 19:15:10 +0800

    Add Income page and Income Table

---
+++ frontend/src/components/IncomeTable.tsx
@@ -0,0 +1,35 @@
+import type { IncomeData } from "../types/IncomeData";
+function populateTableData(incomeList: Array<IncomeData>) {
+  return incomeList.map((income) => (
+    <tr key={income.id} className="hover:bg-gray-100">
+      <td className="px-4 py-2 border text-center">{income.receivedFrom}</td>
+      <td className="px-4 py-2 border text-center">{income.amount}</td>
+      <td className="px-4 py-2 border text-center">{income.account}</td>
+      <td className="px-4 py-2 border text-center">{income.category}</td>
+      <td className="px-4 py-2 border text-center">{income.date}</td>
+      <td className="px-4 py-2 border text-center">{income.comments}</td>
+    </tr>
+  ));
+}
+
+export default function IncomeTable({
+  incomeList,
+}: {
+  incomeList: IncomeData[];
+}) {
+  return (
+    <table className="table-auto w-full border border-gray-300">
+      <thead className="bg-gray-200">
+        <tr>
+          <th className="px-4 py-1 border">Received From</th>
+          <th className="px-4 py-1 border">Amount</th>
+          <th className="px-4 py-1 border">Account</th>
+          <th className="px-4 py-1 border">Category</th>
+          <th className="px-4 py-1 border">Date</th>
+          <th className="px-4 py-1 border">Comments</th>
+        </tr>
+      </thead>
+      <tbody>{populateTableData(incomeList)}</tbody>
+    </table>
+  );
+}
---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div className="flex flex-row lg:gap-x-12">
+      <h1 className="font-bold">Expenses by Category</h1>
+      <h1 className="font-bold">Income vs Expense</h1>
+    </div>
+  );
+}
